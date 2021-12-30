40128f1ba73bb0c2636deaf0990f041f719c0149
Peter Mitrano <mitranopeter@gmail.com>
2021-12-30 14:57:25 -0500

    new api and lambda function for sync

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function SyncButton(props) {
+    return (
+        <button onClick={() => fetch('localhost:5000/sync').then(res => res.json())
+            .then((res) => {
+                console.log("Created at " + res['created-at'])
+                props.text = res['text']
+            })
+            .catch(console.log)}>
+            Sync & Regenerate
+        </button>
+    );
 }
+
+function ReadingList(props) {
+    return <h1>Reading List</h1>
+}
+
+function App() {
+    return (
+        <div className="App">
+            <SyncButton/>
+            <ReadingList/>
+        </div>
+    );
+}
+
 
 export default App;
